d5100ba1b8d5f96511b767efe040cdf0269aeb3a
adityamaksare <adityamaksare@outlook.com>
2024-09-05 22:15:56 +0530

    navigation and hero section completed

--- capstone-project/src/Hero.jsx
+++ capstone-project/src/Hero.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import OverlapImage from "./Assets/restaurantfood.jpg";
 
 export const Hero = () => {
   return (
@@ -6,16 +7,22 @@ export const Hero = () => {
       <div className="container p-0 ">
         <div className="row m-0">
           <div className="col-12 col-md-6 d-flex flex-column align-items-start justify-content-left p-0">
-            <h1 className="m-0 little-lemon-heading">Little Lemon</h1>
+            <h1 className="m-0 little-lemon-heading mt-5">Little Lemon</h1>
             <h2 className="hero-chicago">Chicago</h2>
             <p className="brand-statement">
-              We are family owned Mediterrinean restaurant, focused on
-              traditional recipes and served with modern twist.
+              We are family owned Mediterranean restaurant, focused on
+              traditional recipes and served with a modern twist.
             </p>
             <button>Reserve a table</button>
           </div>
-          <div className="col-12 col-md-6 d-flex align-items-center justify-content-left p-0">
-            right section
+
+          {/* Image Overlapping Section */}
+          <div className="col-12 col-md-6 d-flex align-items-center justify-content-center p-0 image-container position-relative">
+            <img
+              src={OverlapImage}
+              alt="Restaurant Overlap"
+              className="img-fluid overlap-img"
+            />
           </div>
         </div>
       </div>
