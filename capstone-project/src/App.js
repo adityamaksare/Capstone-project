6685c1cf6dc9d7c2fd4f5eb0a02496d05c38f166
adityamaksare <adityamaksare@outlook.com>
2024-09-05 23:55:20 +0530

    Navbar and Hero section complete

--- capstone-project/src/App.js
+++ capstone-project/src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import { Navbar } from './Navbar';
 import { Hero } from './Hero';
+import { Highlights } from './Highlights';
+
 
 function App() {
   return (
     <div className="App container-fluid p-0">
      <Navbar/>
      <Hero/>
+     <Highlights/>
     </div>
   );
 }
---
+++ capstone-project/src/Highlights.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const MenuCardComponent = () => {};
+
+export const Highlights = () => {
+  return (
+    <div className="container highlights p-0">
+      <h1>This Week Special!</h1>
+    </div>
+  );
+};
